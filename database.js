899349b7c32cbd2dff551daea29fdfe1896b1129
EC2 Default User <ec2-user@ip-172-31-40-250.ec2.internal>
2021-04-22 19:11:16 +0000

    Changes to Cancel Reservation Logic

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const { Pool} = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'db.c1mxmtuo85dq.us-east-1.rds.amazonaws.com',
-  database: 'db',
+  database: 'db_cancel',
   password: 'admin123',
   port: 5432,
 })
